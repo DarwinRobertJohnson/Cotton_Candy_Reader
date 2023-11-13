4c4dd963081fa3cf2a4b8f9901834cf66644a5aa
Darwin <Darwin@DESKTOP-DBFPQMN>
2023-11-13 15:16:17 +0530

    figure out Table Of Content Mechansim

--- src/main/java/cottoncandy/Main.java
+++ src/main/java/cottoncandy/Main.java
@@ -1,14 +1,20 @@
 package cottoncandy;
 
 import cottoncandy.epubHandler.Reader;
+import nl.siegmann.epublib.domain.Resource;
+import nl.siegmann.epublib.domain.TOCReference;
 
-public class Main {
+public class Main{
 	
 	public static void main(String[] args) throws Exception{
 		Reader epubReader=new Reader();
 		System.out.println("Object created Successfully");
 		
-		System.out.println(epubReader.getAuthorName());
+		//System.out.println(epubReader.getAuthorName());
+
+		for(TOCReference r:epubReader.getTableOfContents().getTocReferences()) {
+			System.out.println(r);
+		}
 	}
 
 }
--- src/main/java/cottoncandy/epubHandler/Reader.java
+++ src/main/java/cottoncandy/epubHandler/Reader.java
@@ -1,11 +1,11 @@
 package cottoncandy.epubHandler;
 
+
 import java.io.FileInputStream;
-
-
 
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Metadata;
+import nl.siegmann.epublib.domain.TableOfContents;
 import nl.siegmann.epublib.epub.EpubReader;
 
 public class Reader {
@@ -13,15 +13,19 @@ public class Reader {
 	EpubReader read;
 	Book currentBook;
 	Metadata bookMetaData;
-	
+	TableOfContents ToC;
 	
 	public Reader() throws Exception{
 		read= new EpubReader();
-		currentBook=read.readEpub(new FileInputStream("/home/darwin/Downloads/BoG1"));
+		currentBook=read.readEpub(new FileInputStream("D:\\Books\\Battle Through The Heavens\\Battle Through the Heavens 1-100.epub"));
 		bookMetaData=currentBook.getMetadata();
 	}
 	
 	public String getAuthorName() {
 		return bookMetaData.getFirstTitle();
+	}
+	
+	public TableOfContents getTableOfContents() {
+		return currentBook.getTableOfContents();
 	}
 }
